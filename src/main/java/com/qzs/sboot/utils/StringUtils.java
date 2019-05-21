64e8eaf784ebb945b31ebe7e2112757a08e68154
qzsang <631706099@qq.com>
2019-05-21 14:12:30 +0800

    去掉多余的

--- src/main/java/com/qzs/sboot/utils/StringUtils.java
+++ src/main/java/com/qzs/sboot/utils/StringUtils.java
@@ -1,10 +1,9 @@
 package com.qzs.sboot.utils;
 
-import com.sun.istack.internal.Nullable;
 
 public class StringUtils {
 
-    public static boolean isEmpty(@Nullable CharSequence str) {
+    public static boolean isEmpty(CharSequence str) {
         return str == null || str.length() == 0;
     }
 }
